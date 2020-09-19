eba56b0ebb6148f49afd5fb829be35f513922b25
z-9527 <137596665@qq.com>
2020-09-19 18:18:48 +0800

    重置app文件

--- src/index.tsx
+++ src/index.tsx
@@ -1,16 +1,5 @@
-import React, { Suspense } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom';
-import Test from './components/Test';
-
-interface IProps {}
-
-function App(props: IProps) {
-  return (
-    <Suspense fallback={null}>
-      hello world
-      <Test />
-    </Suspense>
-  );
-}
+import App from './app';
 
 ReactDOM.render(<App />, document.getElementById('app'));
